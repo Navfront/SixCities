6246680397776db1d58c754057f67a656322b642
Navfront <b3rt@yandex.ru>
2022-06-16 00:04:51 +0300

    Создал компонент карточки, прокинул пропсы.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,9 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+type Place = {
+  name: string,
+  type: string,
+  priceValue: number,
+  priceText: string,
+  mark: string,
+  imageURL: string,
+  isBookmarked: boolean,
+  rating: number,
+  link: string,
+}
+
+const PLACES: Place[] = [
+  {
+    name: `Beautiful & luxurious apartment at great location`,
+    type: 'Apartment',
+    priceValue: 120,
+    priceText: 'night',
+    mark: 'Premium',
+    imageURL: 'img/apartment-01.jpg',
+    isBookmarked: Boolean(Math.round(Math.random())),
+    rating: Math.round(Math.random() * 100),
+    link: '#'
+  }
+
+]
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App places={PLACES} />
   </React.StrictMode>,
   document.getElementById('root'),
 );
